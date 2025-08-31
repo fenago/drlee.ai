9fd9995a3e8538a322292f8e1b31e9af1b483733
Your Name <your.email@example.com>
2025-08-31 15:19:58 -0400

    fixing modules

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,6 +1,9 @@
-const { createRequestHandler } = require("@remix-run/node");
+import { createRequestHandler } from "@remix-run/node";
 
-exports.handler = createRequestHandler({
-  build: require("../../build/server/index.js"),
+// Use dynamic import to handle the server build with top-level await
+const build = await import("../../build/server/index.js");
+
+export const handler = createRequestHandler({
+  build,
   mode: process.env.NODE_ENV,
 });
