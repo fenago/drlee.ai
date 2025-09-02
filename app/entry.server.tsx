75e650f0dc2c398062afd63d2c58a3a08ea79420
Your Name <your.email@example.com>
2025-09-02 01:35:35 -0400

    Fixed TypeScript errors and added Google Analytics integration

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -2,7 +2,7 @@ import type { AppLoadContext, EntryContext } from '@remix-run/node';
 import { RemixServer } from '@remix-run/react';
 import { renderToString } from 'react-dom/server';
 
-export default async function handleRequest(
+export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
